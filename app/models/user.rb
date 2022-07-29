ceb3572c8be2edb5068899749815a5e26639fb28
Isabel <isanpe@gmail.com>
2022-07-29 02:49:44 -0400

    Agrega modelo user mediante devise

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+# Include default devise modules. Others available are:
+# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, :last_name, :rut, presence: true
+
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: %i[user admin]       
+end
