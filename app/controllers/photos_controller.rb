aabd0b74eefa8b0221ae53a1fb861d5a166c7381
Isabel <isanpe@gmail.com>
2022-08-11 07:13:54 -0400

    Agrega fotos, navbar, active_storage

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -65,6 +65,6 @@ class PhotosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def photo_params
-      params.require(:photo).permit(:url, :comment, :active)
+      params.require(:photo).permit(:comment, :active, :image )
     end
 end
