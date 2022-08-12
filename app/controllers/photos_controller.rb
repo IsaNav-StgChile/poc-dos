c4da65525dc20b108ad2fc24b4ae0e0f26cc288e
Isabel <isanpe@gmail.com>
2022-08-12 04:15:13 -0400

    Actualiza códigos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -65,6 +65,6 @@ class PhotosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def photo_params
-      params.require(:photo).permit(:comment, :active, :image )
+      params.require(:photo).permit(:comment, :active, :image)
     end
 end
